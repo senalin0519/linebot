20dfa0f45d9473af5ad79bb3090768ca90d348fc
senalin0519 <senalin0519@gmail.com>
2025-03-22 23:20:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -54,5 +54,5 @@ def handle_message(event):
     )
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5001))
+    port = int(os.environ.get("PORT", 5002))
     app.run(host='0.0.0.0', port=port)
